fb266509032e0fb287a7d3afa645cca4669286c8
0xilis <109871561+0xilis@users.noreply.github.com>
2023-01-10 18:17:01 -0500

    accidental int instead of void

--- WFiCloudShortcutFileExporter.m
+++ WFiCloudShortcutFileExporter.m
@@ -2,7 +2,7 @@
 
 @implementation WFiCloudShortcutFileExporter
 /* @class WFiCloudShortcutFileExporter */
--(int)exportWorkflowWithCompletion:(id)arg0 {
+-(void)exportWorkflowWithCompletion:(id)arg0 {
     WFWorkflowRecord *workflowRecord = [arg0 workflowRecord];
     return [[WFGallerySessionManager sharedManager] uploadWorkflow:workflowRecord withName:[workflowRecord name] shortDescription:nil longDescription:nil private:YES completionHandler:nil];
 }
