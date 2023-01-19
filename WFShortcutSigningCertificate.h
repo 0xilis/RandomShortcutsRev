ba71d01e0025bb412cd2b7671c0aa75924a67046
0xilis <109871561+0xilis@users.noreply.github.com>
2023-01-19 16:33:53 -0500

    consistent formatting

--- WFShortcutSigningCertificate.h
+++ WFShortcutSigningCertificate.h
@@ -1,17 +1,12 @@
 #import <Foundation/Foundation.h>
 
 @interface WFShortcutSigningCertificate : NSObject
-
-@property (nonatomic) *__SecCertificate certificate; // ivar: _certificate
+@property (nonatomic) *__SecCertificate certificate;
 @property (readonly, nonatomic) NSString *commonName;
-@property (readonly, nonatomic) NSDate *expirationDate; // ivar: _expirationDate
-
-
+@property (readonly, nonatomic) NSDate *expirationDate;
 -(id)generateAuthData;
 -(id)initWithCertificate:(struct __SecCertificate *)arg0 ;
 -(id)initWithCertificateData:(id)arg0 ;
 -(struct __SecKey *)copyPublicKey;
 -(void)dealloc;
-
-
 @end
