0d657154976cadc1de4a847f7a383452753594b9
0xilis <109871561+0xilis@users.noreply.github.com>
2023-01-19 16:32:55 -0500

    consistent formatting

--- WFShortcutSigningCertificate.m
+++ WFShortcutSigningCertificate.m
@@ -3,24 +3,24 @@
 
 @implementation WFShortcutSigningCertificate
 -(id)generateAuthData {
-  return SecCertificateCopyData([self certificate]);
+ return SecCertificateCopyData([self certificate]);
 }
 -(struct __SecKey *)copyPublicKey {
-    return SecCertificateCopyKey([self certificate]);
+ return SecCertificateCopyKey([self certificate]);
 }
 -(id)initWithCertificate:(struct __SecCertificate *)arg0 {
-    self = [super init];
-    if (self) {
-            self.certificate = arg0;
-    }
-    return self;
+ self = [super init];
+ if (self) {
+  self.certificate = arg0;
+ }
+ return self;
 }
 -(id)initWithCertificateData:(id)arg0 {
-    SecCertificateRef cert = SecCertificateCreateWithData(0, arg0);
-    if (cert) {
-            return [self initWithCertificate:cert];
-    }
-    return 0;
+ SecCertificateRef cert = SecCertificateCreateWithData(0, arg0);
+ if (cert) {
+  return [self initWithCertificate:cert];
+ }
+ return 0;
 }
 
 @end
