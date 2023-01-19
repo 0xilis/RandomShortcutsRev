b3681e82077cf8bcd39d3a9972019203778bbeec
0xilis <109871561+0xilis@users.noreply.github.com>
2023-01-19 13:06:21 -0500

    WFShortcutSigningCertificate.m

--- WFShortcutSigningCertificate.m
+++ WFShortcutSigningCertificate.m
@@ -5,4 +5,22 @@
 -(id)generateAuthData {
   return SecCertificateCopyData([self certificate]);
 }
+-(struct __SecKey *)copyPublicKey {
+    return SecCertificateCopyKey([self certificate]);
+}
+-(id)initWithCertificate:(struct __SecCertificate *)arg0 {
+    self = [super init];
+    if (self) {
+            self.certificate = arg0;
+    }
+    return self;
+}
+-(id)initWithCertificateData:(id)arg0 {
+    SecCertificateRef cert = SecCertificateCreateWithData(0, arg0);
+    if (cert) {
+            return [r19 initWithCertificate:cert];
+    }
+    return 0;
+}
+
 @end
