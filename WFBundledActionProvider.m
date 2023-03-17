71a97563784cc5c46ddb636b35e23f89c6f0b206
0xilis <109871561+0xilis@users.noreply.github.com>
2023-03-17 18:12:16 -0400

    createAllAvailableActionsIncludingMissingActions

--- WFBundledActionProvider.m
+++ WFBundledActionProvider.m
@@ -15,4 +15,23 @@
  }
  return nil;
 }
+-(id)availableActionIdentifiers {
+ return [[NSSet alloc] initWithArray:[[self actionDefinitionsWithIdentifiers:nil]allKeys]];
+}
+-(id)createAllAvailableActionsIncludingMissingActions:(BOOL)includeMissingActions {
+ id actionIds = [self actionDefinitionsWithIdentifiers:nil];
+ NSMutableSet* actions = [NSMutableSet new];
+ for (id actionId actionIds) {
+  id def = [actionIds objectForKey:actionId];
+  id actionWithIdentifier = [self createActionWithIdentifier:actionId definition:def serializedParameters:nil fallbackToMissing:includeMissingActions isForLocalization:NO];
+  if (actionWithIdentifier) {
+   [actions addObject:actionWithIdentifier, def, nil];
+  }
+ }
+ return actions;
+}
+//wrapper methods
+-(id)createAllAvailableActions {
+ return [self createAllAvailableActionsIncludingMissingActions:NO];
+}
 @end
