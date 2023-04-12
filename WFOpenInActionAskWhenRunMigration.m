40c020a8daec3c3369aee81379ee8ae5de263370
0xilis <109871561+0xilis@users.noreply.github.com>
2023-04-12 09:09:17 -0400

    placeholder

---
+++ WFOpenInActionAskWhenRunMigration.m
@@ -0,0 +1,8 @@
+#import "WFOpenInActionAskWhenRunMigration.h"
+
+@interface WFOpenInActionAskWhenRunMigration
++(BOOL)workflowNeedsMigration:(id)idk fromClientVersion:(NSString *)oldVersion {
+}
+-(void)migrateWorkflow {
+}
+@end
