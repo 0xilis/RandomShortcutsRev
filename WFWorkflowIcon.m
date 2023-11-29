96594a276ef7560d7cf3cb5580136508c69a7d0c
0xilis <109871561+0xilis@users.noreply.github.com>
2023-11-29 00:08:27 -0500

    WFWorkflowIcon.m

---
+++ WFWorkflowIcon.m
@@ -0,0 +1,8 @@
+#import "WFWorkflowIcon.h"
+
+@implementation WFWorkflowIcon
+-(instancetype)initWithBackgroundColorValue:(NSInteger)bgColor glyphCharacter:(unsigned short)glyph customImageData:(NSData *)data {
+    /* temp */
+    return nil;
+}
+@end
