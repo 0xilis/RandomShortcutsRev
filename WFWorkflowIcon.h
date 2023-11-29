d4a0fcaff9f3f7ac5d94714f4c6b2f875357cae6
0xilis <109871561+0xilis@users.noreply.github.com>
2023-11-29 00:44:32 -0500

    More WFWorkflowIcon methods

--- WFWorkflowIcon.h
+++ WFWorkflowIcon.h
@@ -2,13 +2,24 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@class WFIcon, WFColor;
+
 @interface WFWorkflowIcon : NSObject
+-(instancetype)init;
+-(instancetype)initWithPaletteColor:(NSUInteger)paletteColor glyphCharacter:(unsigned short)glyph customImageData:(NSData * _Nullable)data;
+-(instancetype)initWithBackgroundColorValue:(NSInteger)bgColor glyphCharacter:(unsigned short)glyph customImageData:(NSData * _Nullable)data;
+-(instancetype)initWithCoder:(NSCoder *)aDecoder;
+-(void)encodeWithCoder:(NSCoder *)aCoder;
+-(WFColor *)backgroundColor;
+@property (readonly, nonatomic) WFIcon *icon;
 //@property (readonly, nonatomic) WFColor *backgroundColor;
-@property (readonly, nonatomic) NSInteger backgroundColorValue; // ivar: _backgroundColorValue
-@property (readonly, copy, nonatomic) NSData *customImageData; // ivar: _customImageData
-@property (readonly, nonatomic) unsigned short glyphCharacter; // ivar: _glyphCharacter
+@property (readonly, nonatomic) NSInteger backgroundColorValue;
+@property (readonly, copy, nonatomic) NSData *customImageData;
+@property (readonly, nonatomic) unsigned short glyphCharacter;
 //@property (readonly, nonatomic) WFIcon *icon;
--(instancetype)initWithBackgroundColorValue:(NSInteger)bgColor glyphCharacter:(unsigned short)glyph customImageData:(NSData *)data;
++(NSUInteger)randomBackgroundColor;
++(unsigned short)defaultGlyphCharacter;
++(BOOL)supportsSecureCoding;
 @end
 
 NS_ASSUME_NONNULL_END
