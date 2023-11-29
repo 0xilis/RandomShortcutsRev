1a46d432841005b47042b124869a9077406f1a1d
0xilis <109871561+0xilis@users.noreply.github.com>
2023-11-29 01:05:21 -0500

    WFIconBackground.m

---
+++ WFIconBackground.m
@@ -0,0 +1,17 @@
+#import "WFIconBackground.h"
+
+@implementation WFIconBackground
+-(instancetype)_init {
+    self = [super init];
+    return self;
+}
+-(instancetype)initWithCoder:(NSCoder *)aDecoder {
+    return [self _init];
+}
+-(void)encodeWithCoder:(id)aCoder {
+    return;
+}
++(BOOL)supportsSecureCoding {
+    return YES;
+}
+@end
