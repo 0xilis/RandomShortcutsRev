5ef7552ef3d9589f49bdf11e86fddccca85d7752
0xilis <109871561+0xilis@users.noreply.github.com>
2024-02-27 13:54:17 -0500

    Fixes to WFShortcutSigningContext

--- WFShortcutSigningContext.m
+++ WFShortcutSigningContext.m
@@ -7,64 +7,90 @@
  */
 
 @implementation WFShortcutSigningContext
--(id)generateAuthData {
- NSArray* certChain = [self signingCertificateChain];
- if ([certChain count]) {
-  long dumb = [[self signingCertificateChain]if_compactMap:^{},wip,wip];
-  if (dumb == [[self signingCertificateChain] count]) {
-   return [NSPropertyListSerialization dataWithPropertyList:[[NSMutableDictionary dictionaryWithDictionary:[NSDictionary dictionaryWithObjects:dumb forKeys:@"SigningCertificateChain" count:1]] format:NSPropertyListBinaryFormat_v1_0 options:nil error:nil]; //this is especially wip
-  } else {
-   //error
-  }
- } else {
-   if ([self appleIDValidationRecord]) {
-     long dumb = [[self appleIDCertificateChain]if_compactMap:^{},wip,wip];
-     if (dumb == [[self appleIDCertificateChain] count]) {
-       SecKeyCopyExternalRepresentation([self signingPublicKey], 0);
-       return [NSPropertyListSerialization dataWithPropertyList:[[NSMutableDictionary dictionaryWithDictionary:[NSDictionary dictionaryWithObjects:dumb,cert,[self signingPublicKeySignature],[[self appleIDValidationRecord]data] forKeys:@"AppleIDCertificateChain",@"SigningPublicKey",@"SigningPublicKeySignature",@"AppleIDValidationRecord" count:4]] format:NSPropertyListBinaryFormat_v1_0 options:nil error:nil]; //this is especially wip
-     } else {
-       //error
-     }
-   } else {
-     return [NSPropertyListSerialization dataWithPropertyList:0 format:NSPropertyListBinaryFormat_v1_0 options:nil error:nil]; //??????
-   }
- }
+-(NSData *)generateAuthData {
+    if ([self signingCertificateChain]) {
+        NSArray *authDataMap = [[self signingCertificateChain]if_compactMap:^(WFShortcutSigningCertificate *cert){
+            return [cert generateAuthData];
+        }];
+        if ([authDataMap count] != [[self signingCertificateChain]count]) {
+            return nil;
+        }
+        NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithDictionary:@{
+            @"SigningCertificateChain" : authDataMap,
+        }];
+        return [NSPropertyListSerialization dataWithPropertyList:dict format:NSPropertyListBinaryFormat_v1_0 options:0 error:nil];
+    } else {
+        NSMutableDictionary *dict;
+        if ([self appleIDValidationRecord]) {
+            NSArray *authDataMap = [[self appleIDCertificateChain]if_compactMap:^(WFShortcutSigningCertificate *cert){
+                return [cert generateAuthData];
+            }];
+            if ([authDataMap count] != [[self appleIDCertificateChain]count]) {
+                return nil;
+            }
+            NSData *signingPublicKey = (__bridge NSData *)SecKeyCopyExternalRepresentation([self signingPublicKey], 0);
+            dict = [NSMutableDictionary dictionaryWithDictionary:@{
+                @"AppleIDCertificateChain" : authDataMap,
+                @"SigningPublicKey" : signingPublicKey,
+                @"SigningPublicKeySignature" : [self signingPublicKeySignature],
+                @"AppleIDValidationRecord" : [self appleIDValidationRecord],
+            }];
+        } else {
+            dict = nil;
+        }
+        return [NSPropertyListSerialization dataWithPropertyList:dict format:NSPropertyListBinaryFormat_v1_0 options:0 error:nil];
+    }
 }
--(BOOL)validateWithSigningMethod:(*NSInteger)arg0 error:(NSError**)arg1 {
-    return [self validateWithSigningMethod:arg0 iCloudIdentifier:nil error:arg1];
+-(BOOL)validateWithSigningMethod:(*NSInteger)arg0 error:(NSError**)err {
+    return [self validateWithSigningMethod:arg0 iCloudIdentifier:nil error:err];
 }
--(struct __SecKey *)copyPublicKey {
+-(SecKeyRef)copyPublicKey {
  if ([[self signingCertificateChain]count]) {
   return [[[self signingCertificateChain]firstObject]copyPublicKey];
  } else {
   return [self signingPublicKey];
  }
 }
--(id)initWithAppleIDValidationRecord:(id)arg0 appleIDCertificateChain:(id)arg1 signingPublicKey:(struct __SecKey *)arg2 signingPublicKeyData:(id)arg3 {
- self = [super init];
- if (self) {
-  self.appleIDValidationRecord = arg0;
-  self.appleIDCertificateChain = arg1;
-  self.signingPublicKey = arg2;
-  self.signingPublicKeySignature = arg3;
- }
- return self;
+-(instancetype)initWithSigningCertificateChain:(NSArray *)signingChain {
+    self = [super init];
+    if (self) {
+        self->_signingCertificateChain = [signingChain copy];
+    }
+    return self;
 }
--(id)initWithSigningCertificateChain:(id)arg0 {
- self = [super init];
- if ((self) && ([arg0 count])) {
-  self.signingCertificateChain = arg0;
- }
- return self;
+-(void)validateAppleIDValidationRecordWithCompletion:(void(^)(BOOL success, int options, NSString * _Nullable icId, NSError *validationError))comp {
+    WFSecurityLog("Validating AppleID Validation Record");
+    SFAppleIDClient *client = [[SFAppleIDClient alloc]init];
+    SFAppleIDAccount *account = [client myAccountWithError:nil];
+    NSString *userDSID = [account altDSID];
+    if ([userDSID isEqualToString:[[self appleIDValidationRecord]altDSID]]) {
+        /* Shared by the user themselves, allow import */
+        WFSecurityLog("Found the current user's AppleID Validation Record");
+        comp(1, 3, 0, 0);
+    } else {
+        if ([WFSharingSettings isPrivateSharingEnabled]) {
+            NSString *emailHashString = WFCombinedHashStringFromArray([[self appleIDValidationRecord] validatedEmailHashes]);
+            NSString *phoneHashString = WFCombinedHashStringFromArray([[self appleIDValidationRecord] validatedPhoneHashes]);
+            SFClient *sfclient = [[SFClient alloc]init];
+            [sfclient contactIDForEmailHash:emailHashString phoneHash:phoneHashString completion:^(BOOL success){
+                NSError *err = nil;
+                if (success) {
+                    WFSecurityLog("Found contact matching with AppleID Validation Record");
+                } else {
+                    err = [NSError errorWithDomain:@"WFWorkflowErrorDomain" code:0x5 userInfo:@{
+                        NSLocalizedDescriptionKey : WFLocalizedString(@"This shortcut cannot be opened because it was shared by someone who is not in your contacts."),
+                    }];
+                    WFSecurityLog("Contact with matching AppleID Validation Record Couldn't be found");
+                }
+                comp(success, 2, 0, err);
+            }];
+        } else {
+            WFSecurityLog("Skipping AppleID Validation Record due to Private Sharing Disabled");
+            comp(0, 2, 0, [WFSharingSettings privateSharingDisabledErrorWithShortcutName:nil]);
+        }
+    }
 }
--(void)validateAppleIDValidationRecordWithCompletion:(id)completion {
- SFAppleIDAccount* account = [[[SFAppleIDClient alloc]init]myAccountWithError:nil];
- if ([[account altDSID]isEqualToString:[[self appleIDValidationRecord]altDSID]]) {
-  completion(0x1,0x3,0x0,0x0);
- } else if ([WFSharingSettings isPrivateSharingEnabled]) {
-  [[[SFClient alloc]init]contactIDForEmailHash:[[self appleIDValidationRecord]validatedEmailHashes] phoneHash:_WFCombinedHashStringFromArray([[self appleIDValidationRecord] validatedPhoneHashes]) completion:^{/*block*/}];
- }
-}
+/* This is an awful decomp of the iCloud verification I attempted maybe about 6-12(?) months back when I was much less skilled. Ignore this... */
 -(BOOL)validateSigningCertificateChainWithICloudIdentifier:(*id)arg0 error:(*id)arg1 {
  //log
  NSArray *signingCertificateChain = [self signingCertificateChain];
@@ -95,39 +121,63 @@
   return YES;
  }
 }
+static __attribute__((always_inline)) BOOL WFAppleIDVerifyCertificateChain(NSArray *certificates) {
+    /* TODO: While logs are implemented, NSErrors are not implemented ATM. */
+    SecPolicyRef policy = SecPolicyCreateAppleIDAuthorityPolicy();
+    SecPolicySetOptionsValue(policy,kSecPolicyCheckTemporalValidity,kCFBooleanFalse);
+    if (policy) {
+        SecTrustRef trust;
+        OSStatus res = SecTrustCreateWithCertificates((__bridge CFArrayRef)certificates, policy, &trust);
+        if (res == 0) {
+            if (trust) {
+                CFErrorRef trustErr;
+                if (SecTrustEvaluateWithError(trust, &trustErr) == 0) {
+                    CFErrorDomain domain = CFErrorGetDomain(trustErr);
+                    if (CFEqual(domain, NSOSStatusErrorDomain)) {
+                        if (CFErrorGetCode(trustErr) == errSecCertificateExpired) {
+                            /* cert is valid if we reached here */
+                            return YES;
+                        } else {
+                            WFSecurityErrorF("Signed Shortcut File Apple ID Certificate Chain Verification: SecTrustEvaluateWithError failed with error %@",trustErr);
+                        }
+                    } else {
+                        WFSecurityErrorF("Signed Shortcut File Apple ID Certificate Chain Verification: SecTrustEvaluateWithError failed with error %@",trustErr);
+                    }
+                } else {
+                    /* cert is valid if we reached here */
+                    return YES;
+                }
+            } else {
+                WFSecurityError("Signed Shortcut File Apple ID Certificate Chain Verification: SecTrustCreateWithCertificates returned NULL trust");
+            }
+        } else {
+            WFSecurityErrorF("Signed Shortcut File Apple ID Certificate Chain Verification: SecTrustCreateWithCertificates failed with error %d",res);
+        }
+    } else {
+        WFSecurityError("Signed Shortcut File Apple ID Certificate Chain Verification: SecPolicyCreateAppleIDAuthorityPolicy failed");
+    }
+    return NO;
+}
 -(BOOL)validateAppleIDCertificatesWithError:(NSError**)err {
-    /* I haven't bothered to fill err yet but validation should be identical */
+    /* TODO: While logs are implemented, NSErrors are not implemented ATM. */
+    WFSecurityLog("Validating AppleID Certificate Chain");
     NSArray <WFShortcutSigningCertificate *>* signingCertificateChain = [self appleIDCertificateChain];
     /* if_map is from IntentsFoundation.framework */
     NSArray* certificates = [signingCertificateChain if_map:^(WFShortcutSigningCertificate *item){
       [item certificate]; //WFShortcutSigningCertificate
     }];
+    BOOL validCertificates = NO;
     if (certificates) {
-        SecPolicyRef policy = SecPolicyCreateAppleIDAuthorityPolicy();
-        SecPolicySetOptionsValue(policy,kSecPolicyCheckTemporalValidity,kCFBooleanFalse);
-        if (policy) {
-            SecTrustRef trust;
-            OSStatus res = SecTrustCreateWithCertificates((__bridge CFArrayRef)certificates, policy, &trust);
-            if (res == 0) {
-                if (trust) {
-                    CFErrorRef trustErr;
-                    if (SecTrustEvaluateWithError(trust, &trustErr) == 0) {
-                        CFErrorDomain domain = CFErrorGetDomain(trustErr);
-                        if (CFEqual(domain, NSOSStatusErrorDomain)) {
-                            if (CFErrorGetCode(trustErr) == errSecCertificateExpired) {
-                                /* cert is valid if we reached here */
-                                return YES;
-                            }
-                        }
-                    } else {
-                        /* cert is valid if we reached here */
-                        return YES;
-                    }
-                }
-            }
-        }
+        validCertificates = WFAppleIDVerifyCertificateChain(certificates);
     }
-    return NO;
+    if (validCertificates) {
+        WFSecurityLog("Shortcut AppleID Certificate Chain Validated Successfully");
+    } else {
+        /*
+         WFSecurityErrorF("Failed to Evaluate AppleID Certificate Chain: %@",verifyErr);
+         */
+    }
+    return validCertificates;
 }
 /* This function is called in preformShortcutDataExtractionWithCompletion */
 -(void)validateWithCompletion:(void(^)(BOOL success, int options, NSString * _Nullable icId, NSError *validationError))comp {
@@ -194,12 +244,43 @@
                         }, nil);
                         Boolean isVerified = SecKeyVerifySignature(publicKey, kSecKeyAlgorithmRSASignatureMessagePSSSHA256, (__bridge CFDataRef)signingPublicKey, (__bridge CFDataRef)signingPublicKeySignature, nil);
                         if (isVerified) {
-                            SFAppleIDValidationRecord *appleIDValidationRecord = dict[@"AppleIDValidationRecord"];
+                            NSData *appleIDValidationRecord = dict[@"AppleIDValidationRecord"];
                             if (appleIDValidationRecord) {
                                 /* also checked for twice? */
                                 appleIDValidationRecord = dict[@"AppleIDValidationRecord"];
                                 if (appleIDValidationRecord) {
-                                   /* finish later */
+                                    dispatch_queue_attr_t attr = dispatch_queue_attr_make_with_qos_class(DISPATCH_QUEUE_SERIAL, QOS_CLASS_USER_INITIATED, 0);
+                                    dispatch_queue_t queue = dispatch_queue_create("SFAppleIDQueue",attr);
+                                    dispatch_semaphore_create(0);
+                                    if (!queue) {
+                                        queue = dispatch_get_global_queue(0, 0);
+                                    }
+                                    SecPolicyRef policy = SecPolicyCreateAppleIDValidationRecordSigningPolicy();
+                                    if (policy) {
+                                        SecPolicySetOptionsValue(policy, kSecPolicyCheckTemporalValidity, kCFBooleanFalse);
+                                        SecTrustRef trust = 0;
+                                        CFDataRef attachedRecordContents = 0;
+                                        if (SecCMSVerifyCopyDataAndAttributes((__bridge CFDataRef)appleIDValidationRecord, 0, policy, &trust, &attachedRecordContents, 0) == 0) {
+                                            if (trust && attachedRecordContents) {
+                                                NSDictionary *authDict = [NSPropertyListSerialization propertyListWithData:(__bridge NSData *)attachedRecordContents options:0 format:0 error:0];
+                                                if (authDict) {
+                                                    SecTrustEvaluateAsync(trust, queue, ^(SecTrustRef  _Nonnull trustRef, SecTrustResultType trustResult) {
+                                                        if ((trustResult == kSecTrustResultProceed) || (trustResult == kSecTrustResultUnspecified)) {
+                                                            /* CFDictionaryGetInt64 is from PrivateFrameworks/CoreUtils.m */
+                                                            uint64_t authVersion = CFDictionaryGetInt64_CarplayImpl((__bridge CFDictionaryRef)authDict, @"Version", nil);
+                                                            if (authVersion >= 0x65) {
+                                                                /* Error, authVersion must be between 1 and 100 */
+                                                            } else {
+                                                                /* Call completion */
+                                                                /* finish later */
+                                                            }
+                                                        }
+                                                    });
+                                                    /* finish later */
+                                                }
+                                            }
+                                        }
+                                    }
                                 }
                             } else {
                                 /* error? */
@@ -217,14 +298,13 @@
     self = [super init];
     if (self) {
         self->_appleIDValidationRecord = record;
-        self->_signingCertificateChain = chain;
+        self->_appleIDCertificateChain = chain;
         self->_signingPublicKey = pubKey;
         self->_signingPublicKeySignature = data;
     }
     return self;
 }
 +(WFShortcutSigningContext *)contextWithAppleIDAccount:(SFAppleIDAccount *)account signingKey:(SecKeyRef)key {
-    /* TODO: Implement logs + errors, for now this decomp doesn't handle errors/logs at all */
     SFAppleIDIdentity *identity = [account identity];
     if (identity) {
         OpaqueSecCertificateRef cert = [[account identity]copyCertificate];
@@ -246,5 +326,8 @@
             }
         }
     }
+}
++(WFShortcutSigningContext *)contextWithSigningCertificateChain:(NSArray *)signingChain {
+    return [[self alloc]initWithSigningCertificateChain:signingChain];
 }
 @end
