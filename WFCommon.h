2cb14ab6c5f6d626d24eee04d726e78c2fa8c13b
0xilis <109871561+0xilis@users.noreply.github.com>
2024-02-29 10:14:03 -0500

    Add general logs

--- WFCommon.h
+++ WFCommon.h
@@ -21,6 +21,7 @@ NSError *WFShortcutPackageFileFailedToExtractShortcutFileError(void);
 NSString *WFCombinedHashStringFromArray(NSArray *array);
 
 extern os_log_t getWFSecurityLogObject(void);
+extern os_log_t getWFGeneralLogObject(void);
 
 #define WFSecurityLog(msg) os_log(getWFSecurityLogObject(), "%s %s", __FUNCTION__, msg);
 #define WFSecurityLogF(fmt, ...) os_log(getWFSecurityLogObject(), "%s " fmt, __FUNCTION__, __VA_ARGS__);
@@ -28,5 +29,12 @@ extern os_log_t getWFSecurityLogObject(void);
 #define WFSecurityInfoF(fmt, ...) os_log_info(getWFSecurityLogObject(), "%s " fmt, __FUNCTION__, __VA_ARGS__);
 #define WFSecurityError(msg) os_log_error(getWFSecurityLogObject(), "%s %s", __FUNCTION__, msg);
 #define WFSecurityErrorF(fmt, ...) os_log_error(getWFSecurityLogObject(), "%s " fmt, __FUNCTION__, __VA_ARGS__);
+
+#define WFGeneralLog(msg) os_log(getWFGeneralLogObject(), "%s %s", __FUNCTION__, msg);
+#define WFGeneralLogF(fmt, ...) os_log(getWFGeneralLogObject(), "%s " fmt, __FUNCTION__, __VA_ARGS__);
+#define WFGeneralInfo(msg) os_log_info(getWFGeneralLogObject(), "%s %s", __FUNCTION__, msg);
+#define WFGeneralInfoF(fmt, ...) os_log_info(getWFGeneralLogObject(), "%s " fmt, __FUNCTION__, __VA_ARGS__);
+#define WFGeneralError(msg) os_log_error(getWFGeneralLogObject(), "%s %s", __FUNCTION__, msg);
+#define WFGeneralErrorF(fmt, ...) os_log_error(getWFGeneralLogObject(), "%s " fmt, __FUNCTION__, __VA_ARGS__);
 
 #endif /* WFCommon_h */
