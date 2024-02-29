6b2d41e4eec6fc5874eda66717c5ead8798f2092
0xilis <109871561+0xilis@users.noreply.github.com>
2024-02-29 10:11:51 -0500

    Add logs

--- WFShortcutExtractor.m
+++ WFShortcutExtractor.m
@@ -7,23 +7,24 @@
 }
 -(void)extractShortcutWithCompletion:(id)comp {
     /* Start extracting a shortcut from file */
+    WFGeneralLog("Start extracting a shortcut from file");
     if ([[self extractingURL]isFileURL] == NO) {
+        WFGeneralLog("Found a remote shortcut URL");
         /* Found a remote shortcut URL */
         [self extractRemoteShortcutFileAtURL:[self extractingURL] completion:comp];
     } else if ([self extractingFile]) {
-        /* Found a shortcut file URL */
+        WFGeneralLog("Found a shortcut file URL");
         [self extractShortcutFile:[self extractingFile] completion:comp];
     } else {
         _extractingFile = [WFFileRepresentation fileWithURL:[self extractingURL] options:[self fileAdoptionOptions]];
         if ([self extractingFile]) {
             [self extractShortcutFile:[self extractingFile] completion:comp];
         } else {
-            /* error */
+            /* TODO: Return error */
         }
     }
 }
 -(void)extractRemoteShortcutFileAtURL:(NSURL *)fileURL completion:(id)comp {
-    /* Downloading a remote shortcut file */
     [[NSURLSession wf_sharedSession]downloadTaskWithURL:fileURL completionHandler:^(NSURL * _Nullable location, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         if (location) {
             WFFileRepresentation *fileRep = [WFFileRepresentation fileWithURL:location options:0x3 ofType:0x0 proposedFilename:[response suggestedFilename]];
@@ -36,9 +37,10 @@
             /* error */
         }
     }];
+    WFGeneralLog("Downloading a remote shortcut file");
 }
 -(void)extractShortcutFile:(WFFileRepresentation *)fileRep completion:(id)comp {
-    /* Extracting a shortcut from file */
+    WFGeneralLog("Extracting a shortcut from file");
     NSData *mappedData = [fileRep mappedData];
     if ([mappedData length] <= 3) {
         /* error */
@@ -59,7 +61,8 @@
     [self extractSignedShortcutFile:fileRep allowsRetryIfExpired:YES completion:comp];
 }
 -(void)extractSignedShortcutFile:(WFFileRepresentation *)fileRep allowsRetryIfExpired:(BOOL)retry completion:(id)comp {
-    /* Extracting a signed shortcut format file */
+    /* Incomplete, the iCloud redownload codepath with an expired cert is not here yet */
+    WFGeneralLog("Extracting a signed shortcut format file");
     WFShortcutPackageFile *package = [[WFShortcutPackageFile alloc]initWithSignedShortcutFileURL:[fileRep fileURL]];
     [package extractShortcutFileRepresentationWithCompletion:^(WFFileRepresentation *outFileRep, long long scFileTypeArg, NSString * icloudId, NSError *err) {
         long long scFileType;
@@ -72,8 +75,9 @@
                 }
                 [self extractWorkflowFile:outFileRep shortcutName:suggestedName shortcutFileContentType:scFileType iCloudIdentifier:icloudId completion:comp];
             } else {
-                /* Expired cert */
+                /* Expired cert, WFErrorIsExpiredCertificateError */
                 if (icloudId) {
+                    WFGeneralLog("Found an iCloud Signed Shortcut File with expired certificate. Trying to download a new one from iCloud")
                     /* Found an iCloud Signed Shortcut File with expired certificate. Trying to download a new one from iCloud */
                 }
             }
@@ -99,8 +103,7 @@
     }];
 }
 -(void)extractWorkflowFile:(WFFileRepresentation *)fileRep completion:(id)comp {
-    /* Extracting an old shortcut format file */
-    /* VCIsInternalBuild() is a function from VoiceShortcutsClient.framework */
+    WFGeneralLog("Extracting an old shortcut format file")
     if ((VCIsInternalBuild() && [WFSharingSettings shortcutFileSharingEnabled]) || [self allowsOldFormatFile]) {
         NSString *suggestedName = [self suggestedName];
         if (suggestedName) {
@@ -113,30 +116,38 @@
         /* [WFSharingSettings shortcutFileSharingDisabledError]; */
     }
 }
--(void)extractWorkflowFile:(WFFileRepresentation*)shortcutFile shortcutName:(NSString *)name shortcutFileContentType:(NSInteger)shortcutType iCloudIdentifier:(id)shortcutIdentifier completion:(id)completion {
- NSError* err = nil;
- WFWorkflowRecord* wfRecord = [[[WFWorkflowFile alloc]initWithDescriptor:[[WFWorkflowFileDescriptor alloc]initWithFile:shortcutFile name:name sourceAppIdentifier:[self sourceApplication]] error:&err]recordRepresentationWithError:&err];
- NSString *source;
- if (shortcutType == 0x1) {
-  source = @"ShortcutSourceFilePublic";
- } else if (shortcutType == 0x2) {
-  source = @"ShortcutSourceFileKnownContacts";
- } else if (shortcutType == 0x3) {
-  source = @"ShortcutSourceFilePersonal";
- } else {
-  source = @"ShortcutSourceUnknown";
- }
- [wfRecord setSource:source];
- if (wfRecord) {
-  WFExtractShortcutResult* extractShortcutResult = [[WFExtractShortcutResult alloc] initWithRecord:wfRecord fileContentType:shortcutType iCloudIdentifier:shortcutIdentifier sourceApplicationIdentifier:[self sourceApplication] sharedDate:[shortcutFile creationDate]];
-  if ([self skipsMaliciousScanning]) {
-   completion(extractShortcutResult, nil);
-  } else {
-   //WFWorkflowRemoteQuarantineRequest / WFRemoteQuarantinePolicyEvaluator stuff
-  }
- } else {
-  completion(nil, err);
- }
+-(void)extractWorkflowFile:(WFFileRepresentation *)fileRep shortcutName:(NSString *)name shortcutFileContentType:(NSInteger)fileType iCloudIdentifier:(NSString *)icloudId completion:(id)comp {
+    WFWorkflowFileDescriptor *fileDesc = [[WFWorkflowFileDescriptor alloc]initWithFile:fileRep name:name sourceAppIdentifier:[self sourceApplication]];
+    NSError *err = nil;
+    WFWorkflowFile *file = [[WFWorkflowFile alloc]initWithDescriptor:fileDesc error:&err];
+    WFWorkflowRecord *record = [file recordRepresentationWithError:&err];
+    NSString *source;
+    if (fileType == 1) {
+        source = @"ShortcutSourceFilePublic";
+    } else if (fileType == 2) {
+        source = @"ShortcutSourceFileKnownContacts";
+    } else if (fileType == 3) {
+        source = @"ShortcutSourceFilePersonal";
+    } else {
+        source = @"ShortcutSourceUnknown";
+    }
+    [record setSource:source];
+    if (record) {
+        WFExtractShortcutResult *result = [[WFExtractShortcutResult alloc]initWithRecord:record fileContentType:fileType iCloudIdentifier:icloudId sourceApplicationIdentifier:[self sourceApplication] sharedDate:[fileRep creationDate]];
+        if ([self skipsMaliciousScanning]) {
+            comp(result, 0);
+        } else {
+            WFWorkflowRemoteQuarantineRequest *req = [[WFWorkflowRemoteQuarantineRequest alloc] initWithWorkflowRecord:record];
+            [[WFRemoteQuarantinePolicyEvaluator sharedEvaluator]evaluatePolicyForRequest:req completion:^(int success){
+                /* I'm guessing here, not that confident with this part */
+                if (success) {
+                    comp(record, 0);
+                } else {
+                    comp(nil, nil)
+                }
+            }];
+        }
+    }
 }
 -(id)initWithURL:(NSURL *)url allowsOldFormatFile:(BOOL)allowOldFileFormat skipsMaliciousScanning:(BOOL)skipScanning fileAdoptionOptions:(NSInteger)options suggestedName:(NSString *)suggestName sourceApplication:(NSString *)app {
  if (!url) {
